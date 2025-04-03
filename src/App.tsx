121a3d8d9f4be0b99c15c2c163fd5cb60edb80c6
rohitjoey <roht.praz@gmail.com>
2025-04-03 15:47:17 +0545

    setup complete

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col min-h-screen grainy">
+        <div className="flex mt-6 h-fit justify-center">
+          <h1 className="text-4xl">Ultimate Football Game Manager</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
