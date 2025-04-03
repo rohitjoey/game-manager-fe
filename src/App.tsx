f95bb68da904020b9c4de56073b0f0924c21a8ea
rohitjoey <roht.praz@gmail.com>
2025-04-03 21:36:32 +0545

    modal approach create game and add player

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,18 @@ interface Players {
   name: string;
 }
 
+interface PlayerStats {
+  id: string;
+  name: string;
+  gamesPlayed: number;
+  gamesWon: number;
+  gamesLost: number;
+  ratio: string;
+  goalsScored: number;
+  goalsConceeded: number;
+  gd: number;
+}
+
 const customStyles = {
   content: {
     top: "50%",
@@ -24,20 +36,39 @@ const customStyles = {
 };
 
 function App() {
+  const initialFormData = {
+    player1Id: "",
+    player2Id: "",
+    score1: 0,
+    score2: 0,
+  };
+
   const [modalIsOpen, setIsOpen] = useState<boolean>(false);
   const [playersList, setPlayersList] = useState<Players[]>([]);
   const [selected1, setSelected1] = useState<string>("");
   const [selected2, setSelected2] = useState<string>("");
   const [disabled1, setDisabled1] = useState<boolean>(false);
   const [disabled2, setDisabled2] = useState<boolean>(false);
+  const [formData, setFormData] = useState(initialFormData);
+  const [createFlag, setCreateFlag] = useState<boolean>(false);
+  const [playerModelOpen, setPlayerModelOpen] = useState<boolean>(false);
+  const [playerNameFormData, setPlayerNameFormData] = useState("");
+
+  const [tableRows, setTableRows] = useState<PlayerStats[]>([]);
+  const [refetchFlag, setRefetchFlag] = useState(false);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchTableData = async () => {
+    const response = await api.get<PlayerStats[]>("/dashboard");
+    setTableRows(response.data);
+  };
 
   async function openModal() {
     setIsOpen(true);
     const response = await api.get<Players[]>("/players");
     setPlayersList(response.data);
   }
-
-  console.log(playersList);
 
   function closeModal() {
     setIsOpen(false);
@@ -47,16 +78,66 @@ function App() {
     setSelected2("");
   }
 
-  const handleSaveGame = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    console.log("asdfd")
+  const handleSaveGame = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const response = await api.post("/game", formData);
+    console.log(response.data);
     setIsOpen(false);
     setDisabled1(false);
     setDisabled2(false);
     setSelected1("");
     setSelected1("");
     setSelected2("");
-    //callAPI
+    setFormData(initialFormData);
+    setRefetchFlag((prev) => !prev);
+  };
+
+  const handleAddPlayer = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const response = await api.post("/players", { name: playerNameFormData });
+      console.log(response.data);
+      setLoading(false);
+      setPlayerModelOpen(false);
+      setRefetchFlag((prev) => !prev);
+      setPlayerNameFormData("")
+    } catch (err) {
+      setLoading(false);
+      setPlayerNameFormData("")
+      setError("An error occurred");
+      setTimeout(() => {
+        setError(null);
+      }, 1000);
+    }
+  };
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: name === "score1" || name == "score2" ? Number(value) : value,
+    }));
+    if (name === "player1Id") {
+      setSelected1(e.target.value);
+      setDisabled1(true);
+    }
+    if (name === "player2Id") {
+      setSelected2(e.target.value);
+      setDisabled2(true);
+    }
+  };
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPlayerNameFormData(e.target.value);
+  };
+
+  const creatGameHandler = () => {
+    openModal();
+    setCreateFlag(true);
   };
 
   return (
@@ -74,15 +155,30 @@ function App() {
             >
               Start game
             </button>
-            <button className="px-4 py-2 bg-red-400 text-black rounded-md hover:bg-red-600 cursor-pointer ">
+            <button
+              onClick={creatGameHandler}
+              className="px-4 py-2 bg-red-400 text-black rounded-md hover:bg-red-600 cursor-pointer "
+            >
               Create game
+            </button>
+            <button
+              onClick={() => setPlayerModelOpen(true)}
+              className="px-4 py-2 bg-red-400 text-black rounded-md hover:bg-red-600 cursor-pointer "
+            >
+              Add player
             </button>
           </div>
         </div>
         <div className="overflow-x-auto rounded-lg border border-gray-200 shadow-md mt-16 mx-8">
-          <DashboardTable />
+          <DashboardTable
+            data={tableRows}
+            refetchFlag={refetchFlag}
+            fetchTableData={fetchTableData}
+          />
         </div>
       </div>
+
+      {/* Create or save game modal */}
       <Modal
         isOpen={modalIsOpen}
         // onAfterOpen={afterOpenModal}
@@ -92,7 +188,9 @@ function App() {
       >
         {/* top content */}
         <div className="flex justify-between">
-          <h2 className="text-3xl">Start Game</h2>
+          <h2 className="text-3xl">
+            {createFlag ? "Create Game" : "Start Game"}
+          </h2>
           <button className="cursor-pointer" onClick={closeModal}>
             <X />
           </button>
@@ -104,14 +202,11 @@ function App() {
               <h1 className="text-center text-2xl">Player1</h1>
               <div className="flex p-4 g-2">
                 <select
-                  onChange={(e) => {
-                    setSelected1(e.target.value);
-                    setDisabled1(true);
-                  }}
+                  name="player1Id"
+                  onChange={handleChange}
+                  value={formData.player1Id}
                   disabled={disabled1}
                   className="border-2 rounded-lg p-2 mr-4 focus:outline-none"
-                  name="cars"
-                  id="cars"
                 >
                   <option value="">Select Player</option>
                   {playersList.map((player) => (
@@ -125,8 +220,11 @@ function App() {
                   ))}
                 </select>
                 <input
-                  defaultValue={0}
+                  min="0"
+                  name="score1"
                   type="number"
+                  onChange={handleChange}
+                  value={formData.score1}
                   className="p-2 w-18 h-8 border-2 rounded-lg text-center focus:outline-none"
                 />
               </div>
@@ -135,19 +233,19 @@ function App() {
               <h1 className="text-center text-2xl">Player2</h1>
               <div className="flex p-4 g-2">
                 <input
-                  defaultValue={0}
                   type="number"
+                  min="0"
+                  name="score2"
+                  onChange={handleChange}
+                  value={formData.score2}
                   className="p-2 w-18 h-8 border-2 rounded-lg text-center focus:outline-none"
                 />
                 <select
                   className="border-2 rounded-lg p-2 ml-4 focus:outline-none"
-                  name="cars"
-                  id="cars"
-                  onChange={(e) => {
-                    setSelected2(e.target.value);
-                    setDisabled2(true);
-                  }}
+                  name="player2Id"
+                  onChange={handleChange}
                   disabled={disabled2}
+                  value={formData.player2Id}
                 >
                   <option value="">Select Player</option>
                   {playersList.map((player) => (
@@ -168,6 +266,46 @@ function App() {
             className="p-2 mt-2 bg-red-300 text-black rounded-md hover:bg-red-700 hover:text-white cursor-pointer "
           >
             Save Game
+          </button>
+        </form>
+      </Modal>
+
+      {/* addplayer modal */}
+      <Modal
+        isOpen={playerModelOpen}
+        // onAfterOpen={afterOpenModal}
+        // onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        {/* top content */}
+        <div className="flex justify-between">
+          <h2 className="text-3xl">Add Player</h2>
+          <button className="cursor-pointer" onClick={()=>setPlayerModelOpen(false)}>
+            <X />
+          </button>
+        </div>
+        {/* middle content */}
+        <form onSubmit={handleAddPlayer}>
+          <div className="flex mt-6">
+            <div className="flex flex-col">
+              <h1 className="text-2xl">Name</h1>
+
+              <input
+                name="playername"
+                onChange={handleNameChange}
+                value={playerNameFormData}
+                className="p-2 w-full h-8 border-2 rounded-lg  focus:outline-none my-4"
+              />
+            </div>
+          </div>
+          {error}
+          <button
+            type="submit"
+            disabled={error ? true : false}
+            className="p-2 mt-2 bg-red-300 text-black rounded-md hover:bg-red-700 hover:text-white cursor-pointer "
+          >
+            Add Player
           </button>
         </form>
       </Modal>
