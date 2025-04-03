7169ab08e327d5460a21febacfdddf2f84efa5cc
rohitjoey <roht.praz@gmail.com>
2025-04-03 16:03:15 +0545

    Dashboard table data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { DashboardTable } from "./component/DashboardTable";
 
 function App() {
   return (
@@ -6,6 +7,9 @@ function App() {
       <div className="flex flex-col min-h-screen grainy">
         <div className="flex mt-6 h-fit justify-center">
           <h1 className="text-4xl">Ultimate Football Game Manager</h1>
+        </div>
+        <div className="overflow-x-auto rounded-lg border border-gray-200 shadow-md mt-16 mx-8">
+          <DashboardTable />
         </div>
       </div>
     </>
---
+++ src/component/DashboardTable.tsx
@@ -0,0 +1,119 @@
+import { useEffect, useState } from "react";
+import api from "../config/axios";
+
+interface PlayerStats {
+  id: string;
+  name: string;
+  gamesPlayed: number;
+  gamesWon: number;
+  gamesLost: number;
+  ratio: string;
+  goalsScored: number;
+  goalsConceeded: number;
+  gd: number;
+}
+
+export const DashboardTable = () => {
+  const [rows, setRows] = useState<PlayerStats[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await api.get<PlayerStats[]>("/dashboard");
+        setRows(response.data);
+      } catch (err) {
+        setError(
+          err instanceof Error ? err.message : "An unknown error occurred"
+        );
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-64">
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="flex justify-center items-center h-64">
+        <p>{error}</p>
+      </div>
+    );
+  }
+  return (
+    <>
+      <table className="min-w-full divide-y divide-gray-300">
+        <thead>
+          <tr>
+            <th className="px-6 py-3 text-center text-sm font-semibold bg-red-300">
+              Player name
+            </th>
+            <th className="px-6 py-3 text-center text-sm font-semibold bg-red-200">
+              Games played
+            </th>
+            <th className="px-6 py-3 text-center text-sm font-semibold bg-red-300">
+              Wins
+            </th>
+            <th className="px-6 py-3text-center text-sm font-semibold bg-red-200">
+              Losses
+            </th>
+            <th className="px-6 py-3 text-center text-sm font-semibold bg-red-300">
+              Ratio (Games Played/Win)
+            </th>
+            <th className="px-6 py-3 text-center text-sm font-semibold bg-red-200">
+              Goals For
+            </th>
+            <th className="px-6 py-3 text-center text-sm font-semibold bg-red-300">
+              Goals Against
+            </th>
+            <th className="px-6 py-3 text-center text-sm font-semibold bg-red-200">
+              Goals Difference
+            </th>
+          </tr>
+        </thead>
+
+        <tbody className="divide-y divide-black bg-white">
+          {rows.map((row) => (
+            <tr key={row.name} className="hover:bg-gray-50">
+              <td className="whitespace-nowrap text-center px-6 py-4 text-sm ">
+                {row.name}
+              </td>
+              <td className="whitespace-nowrap text-center px-6 py-4 text-sm ">
+                {row.gamesPlayed}
+              </td>
+              <td className="whitespace-nowrap text-center px-6 py-4 text-sm ">
+                {row.gamesWon}
+              </td>
+              <td className="whitespace-nowrap text-center px-6 py-4 text-sm ">
+                {row.gamesLost}
+              </td>
+              <td className="whitespace-nowrap text-center px-6 py-4 text-sm ">
+                {row.ratio}
+              </td>
+              <td className="whitespace-nowrap text-center px-6 py-4 text-sm ">
+                {row.goalsScored}
+              </td>
+              <td className="whitespace-nowrap text-center px-6 py-4 text-sm ">
+                {row.goalsConceeded}
+              </td>
+              <td className="whitespace-nowrap text-center px-6 py-4 text-sm ">
+                {row.gd}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
